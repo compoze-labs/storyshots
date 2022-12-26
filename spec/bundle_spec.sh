e7bdfb7ae9d4584ae8ee5252a952d2c6ca115770
Adam Eichelkraut <adam.eichelkraut@compozelabs.com>
2022-12-25 23:30:03 -0600

    feat: storyshots-update can update baselines

--- spec/bundle_spec.sh
+++ spec/bundle_spec.sh
@@ -1,11 +1,27 @@
 Describe 'the bundle'
 
+  reset_repo() {
+    rm -rf ${STORYSHOTS_RESULTS_DIR}/test-results
+
+    git checkout sample-storybook/src/stories/Button.stories.tsx > /dev/null 2>&1
+
+    git checkout ${STORYSHOTS_RESULTS_DIR}/example-button--large-chromium.jpeg > /dev/null 2>&1
+    git checkout ${STORYSHOTS_RESULTS_DIR}/example-button--primary-chromium.jpeg > /dev/null 2>&1
+    git checkout ${STORYSHOTS_RESULTS_DIR}/example-button--secondary-chromium.jpeg > /dev/null 2>&1
+    git checkout ${STORYSHOTS_RESULTS_DIR}/example-button--small-chromium.jpeg > /dev/null 2>&1
+  }
+
   storyshots() {
     ./batect --override-image storyshots=${IMAGE} storyshots
   }
 
+  storyshots_update() {
+    ./batect --override-image storyshots=${IMAGE} storyshots-update
+  }
+
   Describe 'happy paths'
     setup() { 
+      reset_repo
       pnpm -F sample build-storybook > /dev/null 2>&1
     }
     BeforeAll 'setup'
@@ -20,15 +36,14 @@ Describe 'the bundle'
   End
 
   Describe 'failure cases'
-
+  
     setup() { 
-      rm -rf ${STORYSHOTS_RESULTS_DIR}/test-results
-      git checkout sample-storybook/src/stories/Button.stories.tsx > /dev/null 2>&1
+      reset_repo
       git apply spec/ensure_failure.patch
       pnpm -F sample build-storybook > /dev/null 2>&1
     }
     cleanup() {
-      git checkout sample-storybook/src/stories/Button.stories.tsx > /dev/null 2>&1
+      reset_repo
     }
     BeforeAll 'setup'
     AfterAll 'cleanup'
@@ -39,6 +54,17 @@ Describe 'the bundle'
       The stderr should match pattern '*'
       The path ${STORYSHOTS_RESULTS_DIR}/test-results/storyshots-our-visual-regressions-should-match-the-existing-specs-chromium/example-button--primary-diff.jpeg should be file
       The status should be failure
+    End
+
+    It 'can update baselines'
+      When run storyshots_update
+      The output should include '1 passed'
+      The output should include '/storyshots/storyshots.spec.ts-snapshots/example-button--primary-chromium.jpeg does not match, writing actual.'
+      The output should include '/storyshots/storyshots.spec.ts-snapshots/example-button--secondary-chromium.jpeg does not match, writing actual.'
+      The output should include '/storyshots/storyshots.spec.ts-snapshots/example-button--large-chromium.jpeg does not match, writing actual.'
+      The output should include '/storyshots/storyshots.spec.ts-snapshots/example-button--small-chromium.jpeg does not match, writing actual.'
+      The stderr should match pattern '*'
+      The status should be success
     End
 
   End
